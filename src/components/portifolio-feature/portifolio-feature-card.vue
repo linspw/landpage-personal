2e8efdc02ca3f0802a7d37ff0193cc246829fd74
Jessé Correia Lins <linspw@live.com>
2024-03-21 15:34:22 -0300

    update portifolios

---
+++ src/components/portifolio-feature/portifolio-feature-card.vue
@@ -0,0 +1,79 @@
+<template>
+  <v-card
+    :color="color"
+    rounded="xl"
+    class="portifolio-feature-card pa-10 h-100"
+    :href="linkUrl"
+    target="_blank"
+  >
+    <v-card-item class="h-100">
+      <app-image
+        :src="imageUrl"
+        :height="90"
+        :width="300"
+        class="mb-4"
+        :zoomable="false"
+      />
+
+      <p class="font-weight-semi-bold">
+        {{ description }}
+      </p>
+
+      <div class="d-flex flex-wrap gap-1 mt-4" v-if="tags.length">
+        <v-chip v-for="tag in tags" :key="tag">{{ tag }}</v-chip>
+      </div>
+
+      <template #append>
+        <div class="d-flex align-center">
+          <v-icon icon="fas fa-chevron-right" />
+        </div>
+      </template>
+    </v-card-item>
+  </v-card>
+</template>
+
+<script setup lang="ts">
+import type { PropType } from 'vue'
+
+defineProps({
+  linkUrl: {
+    type: String,
+    required: true,
+  },
+  imageUrl: {
+    type: String,
+    required: true,
+  },
+  color: {
+    type: String,
+    default: undefined,
+  },
+  tags: {
+    type: Array as PropType<string[]>,
+    default: () => [],
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+})
+</script>
+
+<style>
+.portifolio-feature-card {
+  & .v-card-item,
+  & .v-card-item__content {
+    height: 100%;
+  }
+
+  & .v-card-item {
+    padding: 0;
+  }
+
+  & .v-card-item__content {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+  }
+}
+</style>
--- src/pages/portifolio.vue
+++ src/pages/portifolio.vue
@@ -3,37 +3,56 @@
     <app-section
       :title="$t('pages.portfolio.title')"
       :description="$t('pages.portfolio.subtitle')"
-      class="fill-height"
+      class="fill-height pb-16"
     >
       <v-container>
+        <h3 class="mb-4">Personal Projects</h3>
+
         <v-row>
           <v-col cols="12" md="6">
-            <v-card
+            <portifolio-feature-card
               color="black"
-              rounded="xl"
-              class="home-page-solutions-section__card pa-10 h-100"
-              href="https://snown.com.br/"
-              target="_blank"
-            >
-              <v-card-item class="h-100">
-                <app-image
-                  src="/partners/snown.svg"
-                  :height="90"
-                  :width="300"
-                  class="mb-4"
-                />
+              link-url="https://snown.com.br/"
+              image-url="/images/portifolio/snown.svg"
+              :description="t('description')"
+              :tags="['private', 'collaboration', 'workspace', 'business']"
+            />
+          </v-col>
 
-                <h4 class="font-weight-semi-bold text-white">
-                  {{ t('description') }}
-                </h4>
+          <v-col cols="12" md="6">
+            <portifolio-feature-card
+              color="white"
+              link-url="https://linspw.github.io/experiment-ui/"
+              image-url="/images/portifolio/experiment-ui-light.svg"
+              :description="t('description')"
+              :tags="['ui', 'public', 'open-source', 'experimental']"
+            />
+          </v-col>
+        </v-row>
+      </v-container>
 
-                <template #append>
-                  <div class="d-flex align-center">
-                    <v-icon icon="fas fa-chevron-right" />
-                  </div>
-                </template>
-              </v-card-item>
-            </v-card>
+      <v-container>
+        <h3 class="mb-4">Open Source Contributions</h3>
+
+        <v-row>
+          <v-col cols="12" md="6">
+            <portifolio-feature-card
+              color="black"
+              link-url="https://snown.com.br/"
+              image-url="/images/portifolio/snown.svg"
+              :description="t('description')"
+              :tags="['private', 'collaboration', 'workspace', 'business']"
+            />
+          </v-col>
+
+          <v-col cols="12" md="6">
+            <portifolio-feature-card
+              color="white"
+              link-url="https://linspw.github.io/experiment-ui/"
+              image-url="/images/portifolio/experiment-ui-light.svg"
+              :description="t('description')"
+              :tags="['ui', 'public', 'open-source', 'experimental']"
+            />
           </v-col>
         </v-row>
       </v-container>
@@ -51,24 +70,7 @@ const { t } = useI18n({
 })
 </script>
 
-<style lang="scss">
-.home-page-solutions-section__card {
-  & .v-card-item,
-  & .v-card-item__content {
-    height: 100%;
-  }
-
-  & .v-card-item {
-    padding: 0;
-  }
-
-  & .v-card-item__content {
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-  }
-}
-</style>
+<style lang="scss"></style>
 
 <i18n lang="json">
 {
